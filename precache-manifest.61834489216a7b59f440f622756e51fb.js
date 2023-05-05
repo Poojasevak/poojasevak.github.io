5f3e0a6063873931102237162596d04005e9eb43
unknown <spooja.uidev@gmail.com>
2023-05-05 09:15:24 +0530

    risk viz project

---
+++ precache-manifest.61834489216a7b59f440f622756e51fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9fdefe004ac4f65057421f6d1df15b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55dc1979ada9afe60696",
+    "url": "/static/css/main.58833401.chunk.css"
+  },
+  {
+    "revision": "f295cb68cb499a84d5af",
+    "url": "/static/js/2.192afa6f.chunk.js"
+  },
+  {
+    "revision": "1e7ce0b5e5c5dbbe2385af5c63cacff7",
+    "url": "/static/js/2.192afa6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55dc1979ada9afe60696",
+    "url": "/static/js/main.bc6de9ba.chunk.js"
+  },
+  {
+    "revision": "4d8d778dd5ccfc0d68b2",
+    "url": "/static/js/runtime-main.49c07d5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
